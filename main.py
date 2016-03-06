b308608e035912a60df5602d5190f6801fbc5b9e
Zamir Merali <zamir.merali@gmail.com>
2016-03-06 02:56:12 -0500

    dropdown added, but its nonfunctional

--- main.py
+++ main.py
@@ -8,31 +8,57 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.textinput import TextInput 
 from kivy.uix.boxlayout import BoxLayout 
 from kivy.uix.button import Button 
+from kivy.uix.dropdown import DropDown 
+from kivy.base import runTouchApp
 
 
 #Define Consult Summary Widget
 class ConsultWidget(Widget):
+	#Define the layouts
 	consultpage = BoxLayout(orientation='vertical')
 	top_bar = BoxLayout(orientation='horizontal')
-	go_shift_summary = Button(text='Back to shift summary')
+
+	#Define the dropdown list
+	consult_dropdown = DropDown()
+	states = ['Pending','In Progress','Complete']
+	def callback(instance):
+		x = btn.text
+	for state in states:
+		btn = Button(text='%r' % state, size_hint_y=None, height=44,)
+		btn.bind(on_release=callback)
+		consult_dropdown.add_widget(btn)
+		mainbutton = Button(text='Consult Status')
+		mainbutton.bind(on_release=consult_dropdown.open)
+		consult_dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
+
+	#Define the buttons
+	##Define the back to shift summary button
+	go_shift_summary = Button(text='Back to Shift Summary')
+	
+	#Define the lables
+	identification = Label(text="ID")
 	story = Label(text="story goes here", font_size=18)
 	exam = Label(text="exam goes here", font_size=18)
 	labs = Label(text="labs go here", font_size=18)
 	imaging = Label(text="imaging goes here", font_size=18)
 	plan = Label(text="plan goes here", font_size=18)
+
+	#Define the widget tree
 	consultpage.add_widget(top_bar)
 	top_bar.add_widget(go_shift_summary)
+	top_bar.add_widget(mainbutton)
+
+	consultpage.add_widget(identification)
 	consultpage.add_widget(story)
 	consultpage.add_widget(exam)
 	consultpage.add_widget(labs)
 	consultpage.add_widget(imaging)
 	consultpage.add_widget(plan)
 
-#Define Shift Summary Widget
-
-#define main app
+#define main app class
 class ScutApp(App):
 	def build(self):
+		#show the consult page
 		return ConsultWidget.consultpage
 
 if __name__ == '__main__':
