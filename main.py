b6203ca9ca7990d02b735478b918ba8ec092fea7
Zamir Merali <zamir.merali@gmail.com>
2016-03-09 00:20:22 -0500

    added abstraction module

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button 
 from kivy.uix.dropdown import DropDown 
 from kivy.base import runTouchApp
+from kivy.uix.textinput import TextInput
+import abstraction
 
+#Create the database
+CreateDatabase
 
 #Define Consult Summary Widget
 class ConsultWidget(Widget):
@@ -43,10 +47,48 @@ class ConsultWidget(Widget):
 	imaging = Label(text="imaging goes here", font_size=18)
 	plan = Label(text="plan goes here", font_size=18)
 
-	#Define the widget tree
+	#Define the widget tree for the consult page
 	consultpage.add_widget(top_bar)
 	top_bar.add_widget(go_shift_summary)
 	top_bar.add_widget(mainbutton)
+
+	consultpage.add_widget(identification)
+	consultpage.add_widget(story)
+	consultpage.add_widget(exam)
+	consultpage.add_widget(labs)
+	consultpage.add_widget(imaging)
+	consultpage.add_widget(plan)
+
+#Define Consult Summary Widget
+class ConsultInputWidget(Widget):
+	#Define the layouts
+	consultpage = BoxLayout(orientation='vertical')
+	top_bar = BoxLayout(orientation='horizontal')
+
+	#Define the buttons
+	##Define the back to shift summary button
+	go_shift_summary = Button(text='Back to Shift Summary')
+	##Define the write consult button
+	write_consult = Button(text='Start the Consult')
+
+	#Define the Text Inputs
+	identification = TextInput(text="ID")
+	identification.bind(text=tempfunction)
+	story = TextInput(text="story goes here", font_size=18)
+	story.bind(text=tempfunction)
+	exam = TextInput(text="exam goes here", font_size=18)
+	exam.bind(text=tempfunction)
+	labs = TextInput(text="labs go here", font_size=18)
+	labs.bind(text=tempfunction)
+	imaging = TextInput(text="imaging goes here", font_size=18)
+	imaging.bind(text=tempfunction)
+	plan = TextInput(text="plan goes here", font_size=18)
+	plan.bind(text=tempfunction)
+
+	#Define the widget tree for the input page
+	consultpage.add_widget(top_bar)
+	top_bar.add_widget(go_shift_summary)
+	top_bar.add_widget(write_consult)
 
 	consultpage.add_widget(identification)
 	consultpage.add_widget(story)
@@ -59,7 +101,7 @@ class ConsultWidget(Widget):
 class ScutApp(App):
 	def build(self):
 		#show the consult page
-		return ConsultWidget.consultpage
+		return ConsultInputWidget.consultpage
 
 if __name__ == '__main__':
 	ScutApp().run()
